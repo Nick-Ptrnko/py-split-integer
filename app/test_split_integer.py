ecadf508ef2194f04a2a8b908f38778497e3cd52
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-12-15 14:14:22 +0200

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,43 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    value = 11
+    number_of_parts = 3
+    goals = split_integer(value, number_of_parts)
+    assert value == sum(goals)
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    value = 6
+    number_of_parts = 2
+    goals = split_integer(value, number_of_parts)
+    assert goals == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    value = 8
+    number_of_parts = 1
+    goals = split_integer(value, number_of_parts)
+    assert goals == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    value = 8
+    number_of_parts = 1
+    goals = split_integer(value, number_of_parts)
+    assert goals == sorted(goals)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    value = 2
+    number_of_parts = 5
+    goals = split_integer(value, number_of_parts)
+    assert goals == [0, 0, 0, 1, 1]
+    assert number_of_parts == len(goals)
+
+
+def test_max_min_1() -> None:
+    value = 32
+    number_of_parts = 6
+    goals = split_integer(value, number_of_parts)
+    assert max(goals) - min(goals) <= 1
